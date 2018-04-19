66a4f490168a66c1bd22558afc82b2e31c842c28
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-04-19 17:26:06 +0200

    Init

---
+++ ultrasonic-sensor.ino
@@ -0,0 +1,14 @@
+#define LEFT_SENSOR
+#define RIGHT_SENSOR
+#define FRONT_SENSOR
+#define BACK_SENSOR
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
